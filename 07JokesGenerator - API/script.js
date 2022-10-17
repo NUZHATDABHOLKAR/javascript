f9b35f0278699b2d74f6b9466b266de6ea3c9784
NUZHAT KASAM  DABHOLKAR <93029178+NUZHATDABHOLKAR@users.noreply.github.com>
2022-10-17 13:37:00 +0530

    Add files via upload

---
+++ 07JokesGenerator - API/script.js
@@ -0,0 +1,16 @@
+const jokeEl = document.getElementById('joke');
+const get_joke = document.getElementById('jokeBtn');
+
+get_joke.addEventListener('click', generateJoke);
+
+generateJoke();
+
+async function generateJoke() {
+	const jokeRes = await fetch('https://icanhazdadjoke.com/', {
+		headers: {
+			'Accept': 'application/json'
+		}
+	});
+	const joke = await jokeRes.json();
+	jokeEl.innerHTML = joke.joke;
+}
